47a80f42706f88df807e0122d6fdd1ae1a40ef52
emrangel <erriick1122@gmail.com>
2021-11-14 23:46:02 -0500

    Cambios codigo

--- Code/Betplay.R
+++ Code/Betplay.R
@@ -13,6 +13,7 @@ help("geom_image")
 #View(equipos)
 #Teams <- read.csv("C:/Users/Mateo/Desktop/Rstudio/Dislike/Fut/Soccer/Datos/Teams", row.names=1)
 #View(Teams)
+#Importante que el directorio del trabajo sea donde están los escudos 
 
 ##Gráfico xG y xGA mean
 {
@@ -52,39 +53,7 @@ ggsave("Graf.png", width = 12.4, height = 8.2)
 
 ###xG y Goles lineal
 
-{setwd("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2")
-
-  #my.theme = list(theme(axis.line.y.left = element_line(size = 3, color = "white"),
-  # axis.line.y.right = element_line(size = 3, color = "white"),
-  # axis.line.x.top =  element_line(size = 3, color = "white"),
-  # axis.line.x.bottom = element_line(size = 3, color = "white")))
-
-  xgbet <- read_excel("df.xlsx",3)
-  xgbetcontra <- read_excel("df.xlsx",4)
-
-  Teams <- read_excel("Teams.xlsx")
-
-
-  equipos<- xgbet %>%
-    inner_join(xgbetcontra, by=c("Equipo"="Equipo"))
-
-  equipos<-equipos %>%
-    inner_join(Teams, by=c("Equipo"="Understat")) %>%
-    mutate(liga="CO_")
-
-  colnames(equipos)[2]="xG"
-
-
-  #df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",df$Teams ,".png",sep="")
-  equipos$badge <- paste("./",equipos$liga,equipos$Code,".png",sep="")
-
-  equipos <- equipos %>%
-    group_by(badge) %>%
-    summarise(xG=sum(xG),xGA=sum(xGA), Goles=sum(Goles), GolesR=sum(GolesR)) %>%
-    ungroup()
-
-
-
+{
   Graf2=ggplot(equipos, aes(x = xG, y = Goles), col.axis = "red") +
     ggtitle(label = "Análisis de xG" ,subtitle = "Comparativa xG y Goles") +
     geom_image(aes(image = badge), size = 0.04,) +
@@ -121,40 +90,7 @@ ggsave("Graf2.png", width = 12.4, height = 8.2)
 ##Gráfico xGA y GolesR
 
 
-{setwd("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2")
-
-  #my.theme = list(theme(axis.line.y.left = element_line(size = 3, color = "white"),
-  # axis.line.y.right = element_line(size = 3, color = "white"),
-  # axis.line.x.top =  element_line(size = 3, color = "white"),
-  # axis.line.x.bottom = element_line(size = 3, color = "white")))
-
-  xgbet <- read_excel("df.xlsx",3)
-  xgbetcontra <- read_excel("df.xlsx",4)
-
-  Teams <- read_excel("Teams.xlsx")
-
-
-  equipos<- xgbet %>%
-    inner_join(xgbetcontra, by=c("Equipo"="Equipo"))
-
-  equipos<-equipos %>%
-    inner_join(Teams, by=c("Equipo"="Understat")) %>%
-    mutate(liga="CO_")
-
-  colnames(equipos)[2]="xG"
-
-
-  #df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",df$Teams ,".png",sep="")
-  equipos$badge <- paste("./",equipos$liga,equipos$Code,".png",sep="")
-
-  equipos <- equipos %>%
-    group_by(badge) %>%
-    summarise(xG=sum(xG),xGA=sum(xGA), Goles=sum(Goles), GolesR=sum(GolesR)) %>%
-    ungroup()
-
-
-
-  Graf1=ggplot(equipos, aes(x = xGA, y = GolesR), col.axis = "red") +
+{  Graf1=ggplot(equipos, aes(x = xGA, y = GolesR), col.axis = "red") +
     ggtitle(label = "Análisis de xGA" ,subtitle = "Comparativa xGA y Goles Recibidos") +
     geom_image(aes(image = badge), size = 0.04,) +
     geom_abline(intercept = 0, slope = 1, color = "gray", linetype = "dashed")+
