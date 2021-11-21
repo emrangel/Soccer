b643f7e70ac7c24c403ad8bc43f22be8b9f9db19
emrangel <erriick1122@gmail.com>
2021-11-21 08:22:46 -0500

    change

--- Code/Ejercicio_Conmebol.R
+++ Code/Ejercicio_Conmebol.R
@@ -12,42 +12,14 @@ library(ggimage)
 ##Saving 12.4 x 8.2 in image##
 help("geom_image")
 
-##Leer logo
+##equipos <- read.csv("C:/Users/Mateo/Desktop/Rstudio/Dislike//Soccer/Datos Conmebol/equiposcon.csv", row.names=1)
+#Importante que el directorio del trabajo sea donde están los escudos 
 
-logo <- image_read("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2/conm.jpg")
 
 ##Gráfico xG y xGA mean
 
 {
-  setwd("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2")
-
-  xgbet <- read_excel("df1.xlsx",3)
-  xgbetcontra <- read_excel("df1.xlsx",4)
-
-  Teams <- read_excel("Teams.xlsx")
-
-
-  equipos<- xgbet %>%
-    inner_join(xgbetcontra, by=c("Equipo"="Equipo"))
-
-  equipos<-equipos %>%
-    inner_join(Teams, by=c("Equipo"="Understat")) %>%
-    mutate(liga="CON_")
-
-  colnames(equipos)[2]="xG"
-
-
-  #df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",df$Teams ,".png",sep="")
-  equipos$badge <- paste("./",equipos$liga,equipos$Code,".png",sep="")
-
-  equipos <- equipos %>%
-    group_by(badge) %>%
-    summarise(xG=sum(xG),xGA=sum(xGA), Goles=sum(Goles), GolesR=sum(GolesR),
-              xGP=sum(xGP),xGAP=sum(xGAP), GolesP=sum(GolesP), GolesRP=sum(GolesRP)) %>%
-    ungroup()
-
-
-
+  
   Graf<-ggplot(equipos, aes(x = xGP, y = xGAP), col.axis = "red") +
     ggtitle(label = "Análisis xG vs xGA" ,subtitle = "Comparativa Promedio por Partido") +
     geom_image(aes(image = badge), size = 0.05,) +
@@ -79,47 +51,13 @@ logo <- image_read("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2/conm
 
 }
 
-#grid::grid.raster(logo, x = 0.85, y = 0.8, just = c('left', 'bottom'), width = unit(1.5, 'inches')) %>%
-
- #dev.off()
-
-#image_write(Graf1,"Graf1.png")
 
 ggsave("CON1.png", width = 12.4, height = 8.2)
 
 
 ###xG y Goles
 
-{setwd("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2")
-
-
-  xgbet <- read_excel("df1.xlsx",3)
-  xgbetcontra <- read_excel("df1.xlsx",4)
-
-  Teams <- read_excel("Teams.xlsx")
-
-
-  equipos<- xgbet %>%
-    inner_join(xgbetcontra, by=c("Equipo"="Equipo"))
-
-  equipos<-equipos %>%
-    inner_join(Teams, by=c("Equipo"="Understat")) %>%
-    mutate(liga="CON_")
-
-  colnames(equipos)[2]="xG"
-
-
-  #df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",df$Teams ,".png",sep="")
-  equipos$badge <- paste("./",equipos$liga,equipos$Code,".png",sep="")
-
-  equipos <- equipos %>%
-    group_by(badge) %>%
-    summarise(xG=sum(xG),xGA=sum(xGA), Goles=sum(Goles), GolesR=sum(GolesR),
-              xGP=sum(xGP),xGAP=sum(xGAP), GolesP=sum(GolesP), GolesRP=sum(GolesRP)) %>%
-    ungroup()
-
-
-
+{
 Graf2=ggplot(equipos, aes(x = xGP, y = GolesP), col.axis = "red") +
     ggtitle(label = "Análisis xG vs Goles" ,subtitle = "Comparativa Promedio por Partido") +
     geom_image(aes(image = badge), size = 0.04,) +
@@ -156,40 +94,7 @@ ggsave("CON2.png", width = 12.4, height = 8.2)
 ##Gráfico xGA y GolesR
 
 
-{setwd("C:/Users/Mateo/Desktop/Rstudio/ObjetivoA/Curso/Modulo 2")
-
-#my.theme = list(theme(axis.line.y.left = element_line(size = 3, color = "white"),
-                     # axis.line.y.right = element_line(size = 3, color = "white"),
-                     # axis.line.x.top =  element_line(size = 3, color = "white"),
-                     # axis.line.x.bottom = element_line(size = 3, color = "white")))
-
-xgbet <- read_excel("df1.xlsx",3)
-xgbetcontra <- read_excel("df1.xlsx",4)
-
-Teams <- read_excel("Teams.xlsx")
-
-
-equipos<- xgbet %>%
-  inner_join(xgbetcontra, by=c("Equipo"="Equipo"))
-
-equipos<-equipos %>%
-  inner_join(Teams, by=c("Equipo"="Understat")) %>%
-  mutate(liga="CON_")
-
-colnames(equipos)[2]="xG"
-
-
-#df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",df$Teams ,".png",sep="")
-equipos$badge <- paste("./",equipos$liga,equipos$Code,".png",sep="")
-
-equipos <- equipos %>%
-  group_by(badge) %>%
-  summarise(xG=sum(xG),xGA=sum(xGA), Goles=sum(Goles), GolesR=sum(GolesR),
-            xGP=sum(xGP),xGAP=sum(xGAP), GolesP=sum(GolesP), GolesRP=sum(GolesRP)) %>%
-  ungroup()
-
-
-
+{
 Graf1=ggplot(equipos, aes(x = xGAP, y = GolesRP), col.axis = "red") +
   ggtitle(label = "Análisis xGA vs Goles Recibidos" ,subtitle = "Comparativa Promedio por Partido") +
   geom_image(aes(image = badge), size = 0.04,) +
@@ -223,5 +128,7 @@ plot(Graf1)
 
 ggsave("CON3.png", width = 12.4, height = 8.2)
 
+##Save data
 
-
+write.csv(equipos, "equiposcon.csv")
+  
